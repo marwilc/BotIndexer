1d43d9c901f3f0d68e70213a317b35bbd3bdf8dc
Marwil Campos <marwilcampos@gmail.com>
2017-10-14 22:24:27 -0400

    implementacion de la interfaz y eventos

--- src/controller/Manager.java
+++ src/controller/Manager.java
@@ -3,6 +3,11 @@
  */
 package controller;
 
+import java.awt.Desktop;
+import java.net.URI;
+
+import view.ViewPrincipal;
+
 /**
  * @author marwilc
  *
@@ -10,10 +15,44 @@ package controller;
 public class Manager
 {
 
+	private ViewPrincipal myViewPrincipal;
+	
 	public Manager()
 	{
 		super();
+		myViewPrincipal = new ViewPrincipal(this);
 		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * This method Open the browser web
+	 * default this pc
+	 * @param url
+	 */
+	public void openBrowser(String url)
+	{
+		// TODO Auto-generated method stub
+		if(java.awt.Desktop.isDesktopSupported()){
+			 try{
+			      Desktop dk = Desktop.getDesktop();
+			      dk.browse(new URI(url));
+			 }catch(Exception error){
+			     System.out.println("Error al abrir URL: "+ error.getMessage());
+			 }
+		}
+	}
+
+	/**
+	 * Este metodo lanza el evento del boton run 
+	 */
+	public void initRunEvent()
+	{
+		// TODO Auto-generated method stub
+		if(!myViewPrincipal.getTextFieldApi().getText().isEmpty()){
+			// open browser google search 
+		}else{
+			System.out.println("Api Key no configurada");
+		}
 	}
 
 	
--- src/view/ViewPrincipal.java
+++ src/view/ViewPrincipal.java
@@ -2,14 +2,22 @@ package view;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Desktop;
 import java.awt.EventQueue;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.net.URI;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.border.EmptyBorder;
+
+import controller.Manager;
+
 import javax.swing.JMenuBar;
 import javax.swing.JMenu;
 import javax.swing.JButton;
@@ -19,8 +27,9 @@ import javax.swing.JLabel;
 import javax.swing.JToggleButton;
 import javax.swing.JTextArea;
 import javax.swing.SwingConstants;
+import java.awt.FlowLayout;
 
-public class ViewPrincipal extends JFrame implements ActionListener
+public class ViewPrincipal extends JFrame implements ActionListener, MouseListener
 {
 
 	private JPanel contentPane;
@@ -34,18 +43,24 @@ public class ViewPrincipal extends JFrame implements ActionListener
 	private JToggleButton tglbtnGoogle;
 	private JToggleButton tglbtnBrowser;
 	private JToggleButton tglbtnBing;
+	private JLabel lblAPI;
+	private Manager myManager;
+	private JPanel jPanelLog;
+	private JLabel lblLog;
 
 	/**
 	 * Create the frame.
 	 */
-	public ViewPrincipal()
+	public ViewPrincipal(Manager manager)
 	{
+		myManager = manager;
 		Font myFontNormal = new Font("Sans", 1, 12);
 		Font myFontCursiva = new Font("Ubuntu", 2, 10);
 		Font myFontLarge = new Font("Ubuntu", 1, 20);
 		
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 500, 550);
+		setVisible(true);
 		setResizable(false);
 		
 		setName("Turbo Indexing v1.0 Alfpha");
@@ -56,18 +71,19 @@ public class ViewPrincipal extends JFrame implements ActionListener
 		setJMenuBar(menuBar);
 		
 		btnRun = new JButton("Run");
+		btnRun.addActionListener(this);
 		btnRun.setBackground(new Color(79, 79, 79));
-		btnRun.setForeground(Color.WHITE);
+		btnRun.setForeground(Color.BLACK);
 		menuBar.add(btnRun);
 		
 		btnPause = new JButton("Pause");
 		btnPause.setBackground(new Color(79, 79, 79));
-		btnPause.setForeground(Color.WHITE);
+		btnPause.setForeground(Color.BLACK);
 		menuBar.add(btnPause);
 		
 		btnStop = new JButton("Stop");
 		btnStop.setBackground(new Color(79, 79, 79));
-		btnStop.setForeground(Color.WHITE);
+		btnStop.setForeground(Color.BLACK);
 		menuBar.add(btnStop);
 		
 		btnPause.addActionListener(this);
@@ -81,8 +97,9 @@ public class ViewPrincipal extends JFrame implements ActionListener
 		contentPane.setLayout(null);
 		
 		textFieldApi = new JTextField();
-		textFieldApi.setForeground(Color.WHITE);
-		textFieldApi.setBounds(32, 47, 143, 19);
+		textFieldApi.setToolTipText("Paste The Api Key");
+		textFieldApi.setForeground(Color.BLACK);
+		textFieldApi.setBounds(32, 45, 143, 25);
 		contentPane.add(textFieldApi);
 		textFieldApi.setColumns(10);
 		
@@ -93,10 +110,9 @@ public class ViewPrincipal extends JFrame implements ActionListener
 		lblTurboIndexing.setBounds(169, 12, 190, 23);
 		contentPane.add(lblTurboIndexing);
 		
-		tglbtnGoogle = new JToggleButton("");
-		tglbtnGoogle.setHorizontalAlignment(SwingConstants.LEFT);
+		tglbtnGoogle = new JToggleButton("On");
 		tglbtnGoogle.setBackground(new Color(79, 79, 79));
-		tglbtnGoogle.setBounds(278, 47, 46, 19);
+		tglbtnGoogle.setBounds(278, 47, 54, 19);
 		contentPane.add(tglbtnGoogle);
 		
 		JLabel lblGoogle = new JLabel("Google:");
@@ -104,9 +120,9 @@ public class ViewPrincipal extends JFrame implements ActionListener
 		lblGoogle.setBounds(213, 49, 61, 15);
 		contentPane.add(lblGoogle);
 		
-		tglbtnBrowser = new JToggleButton("");
+		tglbtnBrowser = new JToggleButton("On");
 		tglbtnBrowser.setBackground(new Color(79, 79, 79));
-		tglbtnBrowser.setBounds(417, 47, 46, 19);
+		tglbtnBrowser.setBounds(417, 47, 54, 19);
 		contentPane.add(tglbtnBrowser);
 		
 		JLabel lblBrowser = new JLabel("Browser:");
@@ -114,9 +130,9 @@ public class ViewPrincipal extends JFrame implements ActionListener
 		lblBrowser.setBounds(342, 49, 71, 15);
 		contentPane.add(lblBrowser);
 		
-		tglbtnBing = new JToggleButton("");
+		tglbtnBing = new JToggleButton("On");
 		tglbtnBing.setBackground(new Color(79, 79, 79));
-		tglbtnBing.setBounds(278, 75, 46, 19);
+		tglbtnBing.setBounds(278, 75, 54, 19);
 		contentPane.add(tglbtnBing);
 		
 		JLabel lblBin = new JLabel("Bing:");
@@ -124,18 +140,28 @@ public class ViewPrincipal extends JFrame implements ActionListener
 		lblBin.setBounds(213, 76, 36, 15);
 		contentPane.add(lblBin);
 		
-		JLabel lblAPI = new JLabel("Under 2Captcha API");
-		lblAPI.setBounds(83, 63, 99, 15);
+		lblAPI = new JLabel("Under 2Captcha API");
+		lblAPI.addMouseListener(this);
+		lblAPI.setForeground(Color.WHITE);
+		lblAPI.setBounds(83, 67, 99, 15);
 		lblAPI.setFont(myFontCursiva);
 		contentPane.add(lblAPI);
 		
+		JScrollPane jScrollPaneEmail =  new JScrollPane();
+		jScrollPaneEmail.setBounds(12, 137, 227, 290);
+		contentPane.add(jScrollPaneEmail);
+		
 		JTextArea textAreaEmail = new JTextArea();
 		textAreaEmail.setBounds(12, 137, 227, 290);
-		contentPane.add(textAreaEmail);
+		jScrollPaneEmail.setViewportView(textAreaEmail);
+		
+		JScrollPane jScrollPaneDomain =  new JScrollPane();
+		jScrollPaneDomain.setBounds(261, 137, 227, 290);	
+		contentPane.add(jScrollPaneDomain);
 		
 		JTextArea textAreaDomain = new JTextArea();
 		textAreaDomain.setBounds(261, 137, 227, 290);
-		contentPane.add(textAreaDomain);
+		jScrollPaneDomain.setViewportView(textAreaDomain);
 		
 		JLabel lblAuthor = new JLabel("by marwilc");
 		lblAuthor.setForeground(Color.WHITE);
@@ -147,11 +173,16 @@ public class ViewPrincipal extends JFrame implements ActionListener
 		lblGit.setBounds(375, 454, 119, 15);
 		contentPane.add(lblGit);
 		
-		btnLog = new JButton("Log");
-		btnLog.setForeground(Color.WHITE);
-		btnLog.setBackground(new Color(79, 79, 79));
-		btnLog.setBounds(12, 439, 61, 25);
-		contentPane.add(btnLog);
+		jPanelLog = new JPanel();
+		jPanelLog.setBackground(new Color(79, 79, 79));
+		jPanelLog.setLayout(null);
+		lblLog = new JLabel("Log: ");
+		lblLog.setBounds(5, 5, 221, 15);
+		lblLog.setHorizontalAlignment(SwingConstants.LEFT);
+		lblLog.setForeground(Color.WHITE);
+		jPanelLog.add(lblLog);
+		jPanelLog.setBounds(12, 439, 359, 25);
+		contentPane.add(jPanelLog);
 		
 		JLabel lblEmail = new JLabel("Email");
 		lblEmail.setForeground(Color.WHITE);
@@ -160,19 +191,19 @@ public class ViewPrincipal extends JFrame implements ActionListener
 		
 		JLabel lblDomain = new JLabel("Domain");
 		lblDomain.setForeground(Color.WHITE);
-		lblDomain.setBounds(257, 123, 70, 15);
+		lblDomain.setBounds(261, 123, 70, 15);
 		contentPane.add(lblDomain);
 		
 		btnCopyTextAreaEmail = new JButton("C");
 		btnCopyTextAreaEmail.setBackground(new Color(79, 79, 79));
-		btnCopyTextAreaEmail.setForeground(Color.WHITE);
-		btnCopyTextAreaEmail.setBounds(193, 121, 46, 19);
+		btnCopyTextAreaEmail.setForeground(Color.BLACK);
+		btnCopyTextAreaEmail.setBounds(193, 117, 46, 19);
 		contentPane.add(btnCopyTextAreaEmail);
 		
 		btnCopyTextAreaDomain = new JButton("C");
 		btnCopyTextAreaDomain.setBackground(new Color(79, 79, 79));
-		btnCopyTextAreaDomain.setForeground(Color.WHITE);
-		btnCopyTextAreaDomain.setBounds(442, 121, 46, 19);
+		btnCopyTextAreaDomain.setForeground(Color.BLACK);
+		btnCopyTextAreaDomain.setBounds(442, 117, 46, 19);
 		contentPane.add(btnCopyTextAreaDomain);
 		
 		
@@ -180,10 +211,64 @@ public class ViewPrincipal extends JFrame implements ActionListener
 		
 	}
 
+	public JTextField getTextFieldApi()
+	{
+		return textFieldApi;
+	}
+
+	public void setTextFieldApi(JTextField textFieldApi)
+	{
+		this.textFieldApi = textFieldApi;
+	}
+
 	@Override
 	public void actionPerformed(ActionEvent e)
 	{
 		// TODO Auto-generated method stub
+		if(e.getSource() == btnRun){
+			myManager.initRunEvent();
+		}
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent e)
+	{
+		// TODO Auto-generated method stub
 		
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e)
+	{
+		// TODO Auto-generated method stub
+		if(e.getSource() == lblAPI){
+			lblLog.setText("Log: 2captcha.com");
+			jPanelLog.repaint();
+			myManager.openBrowser("https://2captcha.com/profile");  
+		}
+		
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e)
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e)
+	{
+		// TODO Auto-generated method stub
+		if(e.getSource() == lblAPI)
+			lblAPI.setForeground(new Color(30, 58, 201));
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e)
+	{
+		// TODO Auto-generated method stub
+		if(e.getSource() == lblAPI)
+			lblAPI.setForeground(Color.WHITE);
 	}
 }
